30e4838d9c69862e12679f0c45ea3f09d75026dd
kalys <jsvsjsvshsksb@gmail.com>
2023-12-15 14:30:31 +0600

    final work

--- snake/main.py
+++ snake/main.py
@@ -1,125 +1,116 @@
-import pygame
-import random
- 
-pygame.init()
- 
-white = (255, 255, 255)
-yellow = (255, 255, 102)
-black = (0, 0, 0)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
- 
-dis_width = 600
-dis_height = 400
- 
-dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game by Kozhomberdiev Kalys EEAIR-23')
- 
-clock = pygame.time.Clock()
- 
-snake_block = 10
-snake_speed = 10
- 
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
- 
- 
-def Your_score(score):
-    value = score_font.render("Your Score: " + str(score), True, yellow)
-    dis.blit(value, [0, 0])
- 
- 
- 
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
- 
- 
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
- 
- 
-def gameLoop():
-    game_over = False
-    game_close = False
- 
-    x1 = dis_width / 2
-    y1 = dis_height / 2
- 
-    x1_change = 0
-    y1_change = 0
- 
-    snake_List = []
-    Length_of_snake = 1
- 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
- 
-    while not game_over:
- 
-        while game_close == True:
-            dis.fill(blue)
-            message("You Lost! Press C-Play Again or Q-Quit", red)
-            Your_score(Length_of_snake - 1)
-            pygame.display.update()
- 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
- 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
- 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        snake_List.append(snake_Head)
-        if len(snake_List) > Length_of_snake:
-            del snake_List[0]
- 
-        for x in snake_List[:-1]:
-            if x == snake_Head:
-                game_close = True
- 
-        our_snake(snake_block, snake_List)
-        Your_score(Length_of_snake - 1)
- 
-        pygame.display.update()
- 
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
- 
-        clock.tick(snake_speed)
- 
-    pygame.quit()
-    quit()
- 
-gameLoop()
+import pygame as pg
+from pygame.math import Vector2
+import sys, random
+
+class FRUIT:
+    def __init__(self):
+        self.randomize()
+
+    def draw_fruit(self):  
+        x_pos = (self.x * cell_size)    # we are multiplying position to get random place on the field
+        y_pos = (self.y * cell_size)
+        fruit_rect = pg.Rect(x_pos, y_pos, cell_size,cell_size)   # defining a fruit view  (position and size)
+        # also the video told me to put values into integers because every value from Vector2 is going to be a float while pygame requires everything to be int, but everything seems okay so i quess it has been fixed
+        screen.blit(apple2,fruit_rect)
+        # pg.draw.rect(screen, (126, 166, 114), fruit_rect)         # drawing the fruit on screen 
+
+    def randomize(self):      # when snake eats our fruit it repositions our fruit
+        self.x = random.randint(0, cell_number-1)                       # defining the position of an object
+        self.y = random.randint(0, cell_number-1)               
+        self.pos = Vector2(self.x, self.y) # Vector unlike an ordinary list can get values by their names, not just by their index
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(7,10), Vector2(6, 10), Vector2(5, 10)]    # defining the length of a snake
+        self.direction = Vector2(1,0)                # direction of a snake (right by default)
+        self.new_block = False
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = (block.x * cell_size)
+            y_pos = (block.y * cell_size)
+            block_rect = pg.Rect(x_pos,y_pos,cell_size,cell_size)
+            pg.draw.rect(screen, (255, 192, 192), block_rect)
+
+    def move_snake(self):
+        if not self.new_block:
+            body_copy = self.body[:-1]           # copiyng each element up to the last
+            body_copy.insert(0,body_copy[0] + self.direction)   # adding a position(x,y) on top of the head
+            self.body = body_copy[:]            # recording new body to the main one
+        else:
+            body_copy = self.body[:]           # instead of getting all elements except for the last one like we do in other case, we are getting an entire body instead
+            body_copy.insert(0,body_copy[0] + self.direction)    # or in other words we are just getting an entire body and it grows 1 block
+            self.body = body_copy[:]
+            self.new_block = False
+
+    def add_tail(self): 
+        self.new_block = True     
+
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+    
+    def update(self):           # checks
+        self.snake.move_snake()    # direction to move
+        self.check_collision()    # if it eats the fruit
+        self.check_lose()         # if it eats itself or touches the border
+
+    def draw_elements(self):
+        self.snake.draw_snake()
+        self.fruit.draw_fruit()
+
+    def check_collision(self):          # when snake eats the fruit
+        if self.fruit.pos == self.snake.body[0]:
+            self.fruit.randomize()      # reposition the fruit
+            self.snake.add_tail()       # growing a block
+
+    def check_lose(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:  # border check
+            self.game_over()
+        for block in self.snake.body[1:]:        # checking each part of the body except for the head
+            if block == self.snake.body[0]:      # if head touches any body part
+                self.game_over()
+
+    def game_over(self):
+        pg.quit()
+        sys.exit()
+
+
+pg.init()                                  # initializing pygame
+cell_size = 40                             # creating a block to fill
+cell_number = 20
+screen = pg.display.set_mode((cell_size * cell_number, cell_size * cell_number))   # creating a screen
+clock = pg.time.Clock()                    # setting up an FPS counter
+apple = pg.image.load('assets/apple.jpg').convert_alpha()    # importing an image
+apple2 = pg.transform.scale(apple, (cell_size, cell_size))                       # resizing it bcs it is huge
+
+main_game = MAIN()
+
+SCREEN_UPDATE = pg.USEREVENT
+pg.time.set_timer(SCREEN_UPDATE, 150)      # creating a gap to move per 150 milliseconds
+
+while True:
+    for event in pg.event.get():           # getting every possible keyword
+        if event.type == pg.QUIT:          # quitting the game
+            pg.quit()
+            sys.exit()
+        if event.type == SCREEN_UPDATE:
+            main_game.update()             
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                if main_game.snake.direction.y != 1:             # if you go reverse immediatly it wont work
+                    main_game.snake.direction = Vector2(0,-1)    # for some wierd reason -1 is upwards
+            if event.key == pg.K_a:
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1,0)    
+            if event.key == pg.K_s:
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0,1)    
+            if event.key == pg.K_d:
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1,0)    
+    screen.fill((144, 238, 144))                 # painting the display (by RGB standart)
+    main_game.draw_elements()
+    pg.display.update()                    # just updating the display after the changes
+    clock.tick(60)                         # ticks 60 times per second
